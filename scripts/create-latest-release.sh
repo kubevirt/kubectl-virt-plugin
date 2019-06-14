5dc5f0046f3c4240161bd9e569c0b609b99daccb
Daniel Hiller <daniel.hiller.1972@googlemail.com>
2019-06-14 12:14:51 +0200

    create-latest-release.sh

---
+++ scripts/create-latest-release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+KUBEVIRT_VERSION=$(curl --fail -s https://api.github.com/repos/kubevirt/kubevirt/releases|grep tag_name|sort -V | tail -1 | awk -F':' '{print $2}' | sed 's/,//' | xargs)
+
+[ -z "$KUBEVIRT_VERSION" ] && (echo "Failed to retrieve latest KubeVirt version!" ; exit 1)
+
+echo "Preparing packages for release $KUBEVIRT_VERSION..."
+
+# shellcheck source=scripts/functions.sh
+"$(dirname "${BASH_SOURCE[0]}")/create-release.sh" "$KUBEVIRT_VERSION"

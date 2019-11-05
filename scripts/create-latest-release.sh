85592ceb466aafdf2735df52df0bea579ecccc8c
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-11-05 08:26:18 +0100

    Check release existence prior to creation

--- scripts/create-latest-release.sh
+++ scripts/create-latest-release.sh
@@ -6,6 +6,23 @@ KUBEVIRT_VERSION=$(curl --fail -s https://api.github.com/repos/kubevirt/kubevirt
 
 [ -z "$KUBEVIRT_VERSION" ] && (echo "Failed to retrieve latest KubeVirt version!" ; exit 1)
 
+
+# /repos/:owner/:repo/releases/tags/:tag
+
+set +e
+release=$(
+    curl --silent --fail \
+        "https://api.github.com/repos/dhiller/kubectl-virt-plugin/releases/tags/$KUBEVIRT_VERSION" \
+        -H 'Content-Type: text/json; charset=utf-8' 2>&1
+)
+result=$?
+set -e
+
+if [ $result -eq 0 ]; then
+    echo "Release $KUBEVIRT_VERSION already exists!"
+    exit 0
+fi
+
 echo "Preparing packages for release $KUBEVIRT_VERSION..."
 
 # shellcheck source=scripts/functions.sh
